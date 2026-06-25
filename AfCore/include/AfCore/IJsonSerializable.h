e5b38b12440937e7ef4456e5a23a309fb5bbf30c
fabien\fdigrande <digrande.fabien@gmail.com>
2026-06-25 17:58:47 +0200

    Add : Ajout de la lib https://github.com/nlohmann/json.git pour sérialiser et désérialiser les classes héritant de IJsonSerializable. Ajout de l'interface.

---
+++ AfCore/include/AfCore/IJsonSerializable.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "CoreConstants.h"
+
+#include <nlohmann/json.hpp>
+#include <memory>
+
+namespace appforge::core {
+
+using json = nlohmann::json;
+
+class AFCORE_EXPORT IJsonSerializable
+{
+public:
+    virtual ~IJsonSerializable() = default;
+
+    // ✅ Serialize
+    virtual json toJson() const = 0;
+
+    // ✅ Deserialize into current instance
+    virtual void fromJson(const json& j) = 0;
+
+    // ✅ Static builder (templated)
+    template<typename T>
+    static std::unique_ptr<T> buildFromJson(const json& j)
+    {
+        static_assert(std::is_base_of<IJsonSerializable, T>::value,
+                      "T must inherit from IJsonSerializable");
+
+        auto instance = std::make_unique<T>();
+        instance->fromJson(j);
+
+        return instance;
+    }
+};
+
+}
